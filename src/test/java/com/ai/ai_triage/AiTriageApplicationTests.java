86acd8f93a5dc7cfeeb9223c077c803e07c304a6
Steph <yvanstephanee@gmail.com>
2026-04-02 09:06:08 +0100

    Initial commit

---
+++ src/test/java/com/ai/ai_triage/AiTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.ai_triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
